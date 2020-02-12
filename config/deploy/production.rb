4d0cbdbd6f6ebe39940cfa0a2407fb97edfb1c79
Vikasvics <Vikasvics19@gmail.com>
2020-02-12 15:54:05 +0530

    Ip change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -63,10 +63,10 @@ set :user, "deploy"
 set :stage, :production
 set :branch, :master
 set :deploy_to, "/u01/apps/qwinix/rails_deploy"
-role :app, %w{deploy@13.233.77.223}
-role :web, %w{deploy@13.233.77.223}
-role :db, %w{deploy@13.233.77.223}
-server '13.233.77.223', roles: %w{:web, :app, :db}, user: 'deploy'
+role :app, %w{deploy@13.235.133.114}
+role :web, %w{deploy@13.235.133.114}
+role :db, %w{deploy@13.235.133.114}
+server '13.235.133.114', roles: %w{:web, :app, :db}, user: 'deploy'
 #before "deploy", "puma:create_puma_bind_file_for_production"
 #after "deploy", "db:migrate_for_production"
 set :ssh_options, {
